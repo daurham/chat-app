5e6045f37e3ca14c7fd2a6223be84c9149dd61a4
Jake <daurham95@gmail.com>
2022-07-10 02:40:37 -0700

    demonstrates websockets and react

--- README.md
+++ README.md
@@ -10,4 +10,4 @@ Simply run and open 2 browser windows on localhost:3000. Both will be able to re
 `npm start`
 #### Start the Client:
 `npm run server`
-#### Open at least 2 browsers on localhost:
+#### Open at least 2 browsers on localhost:3000, & watch the magic.
--- server/index.js
+++ server/index.js
@@ -1,29 +1,20 @@
 const express = require('express');
 const path = require('path');
-// const ctlr = require('./controller');
-
 
 const app = express();  
 const PORT = process.env.USER_PORT || 3000;
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
-// Listeners:
-// app.get('/api', ctlr.getData);
-// app.post('/api', ctlr.postData);
-// app.put('/api', ctlr.updateData);
-// app.delete('/api', ctlr.deleteData);
-
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
 const server = app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
+
 const io = require('socket.io')(server);
+
 io.on('connection', (socket) => {
   console.log('a user connected');
   
-  socket.on('message', (message) => {
+  socket.on('message', (message) => { // 'message' is an event
     console.log(message);
-    io.emit('message', `${socket.id.substr(0, 2)} said ${message}`);
-  }); //message is an event
+    io.emit('message', `${socket.id.substr(0, 2)} said ${message}`); // emit broadcasts to all connected sessions
+  });
 });
